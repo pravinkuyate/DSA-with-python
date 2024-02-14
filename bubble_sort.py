acb1cd4146feee4ca96e5bc95b413ec4da1aa1a0
pravinkuyate <pravinkuyate0@gmail.com>
2024-02-14 14:27:48 +0530

    commit for bubble_sort

---
+++ bubble_sort.py
@@ -0,0 +1,24 @@
+def bubble_sort(elements):                          
+    counter=0
+    size=len(elements)
+    print("size is ",size+1)
+    swapped=False
+    for j in range(int(size/1.7)-1):
+        for i in range(size-1-j):
+            if elements[i] >elements[i+1]:
+            #swap 
+                tmp=elements[i]
+                elements[i]=elements[i+1]
+                elements[i+1]=tmp
+                counter+=1
+                swapped=True
+        if not swapped:
+            break
+    print(counter)
+
+
+if __name__=="__main__":
+
+    elements=[88,78,1,89,56,1,3,54,6,8,9,7,98,95,8,58,1,3]
+    bubble_sort(elements)
+    print(elements)
