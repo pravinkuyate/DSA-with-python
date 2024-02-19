815cfc40828ff11bfb33a067c51aee7a06f73070
pravinkuyate <pravinkuyate0@gmail.com>
2024-02-19 15:40:36 +0530

    commit 19feb2024

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+class stack:
+
+    def __init__(self):
+        self.container=deque()
+    
+    def push(self,val):
+        self.container.append(val)
+    
+    def pop(self):
+        return self.container.pop()
+
+    def is_empty(self):
+        return len(self.container)==0
+    
+    def size(self):
+        return len(self.container)
+    
+
+stack1=stack()
+stack1.push(10)
+
